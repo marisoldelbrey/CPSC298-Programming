c59a12d35079dad83142a61b2c0263951be49a98
Marisol Delbrey <142078599+marisoldelbrey@users.noreply.github.com>
2024-01-13 17:53:14 -0800

    Add files via upload

---
+++ CUltimateMachine.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class CUltimateMachine
+{
+public:
+    CUltimateMachine() : m_bState(false)
+    {
+        std::cout << "CUltimateMachine constructor (ctor) called" << std::endl;
+    }
+
+    ~CUltimateMachine()
+    {
+        std::cout << "CUltimateMachine destructor (dtor) called" << std::endl;
+    }
+
+    void displayState() 
+    {
+        switch (m_bState) {
+        case true:
+            std::cout << "Ultimate Machine is ON" << std::endl;
+            break;
+        case false:
+            std::cout << "Ultimate Machine is OFF" << std::endl;
+        }
+    }
+
+    void turnOn()
+    {
+        m_bState = true;
+        displayState();
+        if (true == m_bState)
+        {
+            m_bState = false;
+        }
+        displayState();
+    }
+
+    private:
+        bool m_bState; // true (ON), false (OFF)
+};
+
+
+int main ()
+{
+    std::cout << "The Ultimate Machine Simulation" << std::endl;
+
+    CUltimateMachine ultmach; 
+    ultmach.displayState();
+    ultmach.turnOn();
+}
